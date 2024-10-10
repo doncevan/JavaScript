d9dbf9ab3c896c8430e5377deec9a632d85e92a1
Ivan D. <doncevan@gmail.com>
2024-10-10 12:23:19 -0400

    Add some nwe code

---
+++ laugh3.js
@@ -0,0 +1,25 @@
+/*
+ * Programming Quiz: Laugh
+ */
+
+/*
+ * QUIZ REQUIREMENTS
+ * - Your code should have a variable `laugh`
+ * - Your code should include an anonymous function expression stored in the variable `laugh`
+ * - Your anonymous function expression should take one argument
+ * - Your anonymous function expression should return the correct number of `hahaha`\'s
+ */
+
+// your code goes here
+const laugh = function (num) {
+  let laughter = "";
+  for (let i = 0; i < num; i++) {
+    laughter += "ha";
+  }
+  return laughter + "!";
+};
+
+// testing your code
+console.log(laugh(1));
+console.log(laugh(5));
+console.log(laugh(10));
---
+++ udacifamily.js
@@ -0,0 +1,17 @@
+/*
+ * Programming Quiz: UdaciFamily (6-1)
+ */
+
+/*
+ * QUIZ REQUIREMENTS
+ * - Your code should have a variable `udaciFamily`
+ * - The variable `udaciFamily` should be an array containing the values `"Julia"`, `"James"`, and one other name
+ * - Your code should print `udaciFamily` to the console
+ */
+
+// your code goes here
+const udaciFamily = ["Julia", "James", "Ivan"];
+/* or const udaciFamily = new Array("Julia", "James", "Ivan"); */
+for (let names = 0; names < udaciFamily.length; names++) {
+  console.log(udaciFamily[names]);
+}
